9b7d0e89db91797ac5aaa587b9967919b7ba38e8
Kevin Hua Jian <huajianchan0206@gmail.com>
2024-04-13 21:50:49 +0800

    chore: Update package name

--- android/app/src/main/kotlin/com/example/see_vee/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/see_vee/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.see_vee
+package com.example.minimalist_cv
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:see_vee/app.dart';
+import 'package:minimalist_cv/app.dart';
 
 void main() {
   runApp(const App());
